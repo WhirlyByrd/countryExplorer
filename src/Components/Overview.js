225a9fdcfbbddefee6cafdb901a15e94108f66ea
WhirlyByrd <kara.r.gardner@gmail.com>
2022-11-09 11:49:31 -0600

    added Symbols functionality

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
+import {selectDisplay} from '../redux/slices/displayCountrySlice'
 
 function Overview() {
+
+    const currentDisplay = useSelector(selectDisplay);
+
   return (
-    <div>Overview</div>
+    <div className='stack'>
+        <h1>{currentDisplay.name.official}</h1>
+        <h2>{currentDisplay.name.common}</h2>
+        <table className='overview-table'>
+        <tbody>
+           <tr>
+                <th>Capitol:</th>
+                {currentDisplay.capital.map((e) => (
+                    <td>{e}</td>))}
+            </tr>   
+            <tr>
+                <th>Population:</th>
+                <td>{currentDisplay.population}</td>
+            </tr> 
+
+            <tr>
+                <th>Region:</th>
+                <td>{currentDisplay.region}</td>
+            </tr> 
+
+            <tr>
+                <th>Subregion:</th>
+                <td>{currentDisplay.subregion}</td>
+            </tr> 
+
+            <tr>
+                <th>UN Member:</th>
+                <td>{currentDisplay.unMember ? 'Yes' : 'No'}</td>
+            </tr> 
+
+           
+            </tbody>
+        </table>
+    </div>
   )
 }
 
--- src/Components/Symbols.js
+++ src/Components/Symbols.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
+import {selectDisplay} from '../redux/slices/displayCountrySlice'
 
 function Symbols() {
+
+    const currentDisplay = useSelector(selectDisplay);
+
   return (
-    <div>Symbols</div>
+    <div className='symbols'>
+        <div className='stack'>
+            <h2>Flag of {currentDisplay.name.common}:</h2>
+            {currentDisplay.flags ? (
+                <img src={currentDisplay.flags.png} alt='flag png'/>
+            ) : (
+                "(No data available)"
+            )}
+        </div>
+        <div className='stack'>
+            <h2>Coat of Arms of {currentDisplay.name.common}:</h2>
+            {currentDisplay.coatOfArms ? (
+                <img src={currentDisplay.coatOfArms.png} alt='coat of arms png'/>
+            ) : (
+                "(No data available)"
+            )}
+        </div>
+    </div>
   )
 }
 
